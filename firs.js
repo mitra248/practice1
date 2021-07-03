946e285a2ff5a32492b23f5e3ee12f709fb9f928
mitra248 <80234277+mitra248@users.noreply.github.com>
2021-07-03 19:39:08 +0530

    Add files via upload

---
+++ firs.js
@@ -0,0 +1,16 @@
+let myage = 18
+let humanDogRatio = 7
+myage = 19
+let MyDogAge = myage * humanDogRatio
+console.log(myage)
+console.log(MyDogAge)
+
+
+let bonuspoints = 50
+console.log(bonuspoints)
+bonuspoints = bonuspoints + 50
+console.log(bonuspoints)
+bonuspoints = bonuspoints - 75
+console.log(bonuspoints)
+bonuspoints = bonuspoints + 45
+console.log(bonuspoints)
